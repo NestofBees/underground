6616a87e6117636a55ccf288ff6b0a5167ea3df1
Jinof <1474121785@qq.com>
2022-09-24 15:42:41 +0000

    :white_check_mark: 测试添加 assert 函数

--- server/server_test.go
+++ server/server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"errors"
 	"net"
 	"reflect"
 	"testing"
@@ -20,28 +21,52 @@ func TestServer(t *testing.T) {
 
 	t.Run("Test connection", func(t *testing.T) {
 		conn, err := net.Dial("tcp", ":8080")
-		if err != nil {
-			t.Fatalf("got %s, expecte err to be nil", err.Error())
-		}
+		assertErrorEqual(t, nil, err)
 		defer conn.Close()
 	})
 
 	t.Run("Test send message", func(t *testing.T) {
+		conn, err := net.Dial("tcp", ":8080")
+		assertErrorEqual(t, nil, err)
+		defer conn.Close()
+
+		data := []byte("Hello world")
+		n, err := conn.Write(data)
+		assertErrorEqual(t, nil, err)
+		assertIntEqual(t, len(data), n)
+	})
+
+	t.Run("Test save message", func(t *testing.T) {
 		conn, err := net.Dial("tcp", ":8080")
 		if err != nil {
 			t.Fatalf("got %s, expecte err to be nil", err.Error())
 		}
 		defer conn.Close()
 
-		_, err = conn.Write([]byte("Hello, World!"))
-		if err != nil {
-			t.Fatalf("got %s, expecte err to be nil", err.Error())
-		}
+		data := []byte("Hello world")
+		n, err := conn.Write(data)
+		assertErrorEqual(t, nil, err)	
+		assertIntEqual(t, len(data), n)
 		asserStringEqual(t, "Hello, World!", buffer.String())
 	})
 }
 
+func assertErrorEqual(t *testing.T, want, got error) {
+	t.Helper()
+	if !errors.Is(want, got) {
+		t.Fatalf("got %s, expected %s", got, want)
+	}
+}
+
+func assertIntEqual(t testing.TB, want, got int) {
+	t.Helper()
+	if want != got {
+		t.Fatalf("got %d, expected %d", got, want)
+	}
+}
+
 func asserStringEqual(t *testing.T, want, got string) {
+	t.Helper()
 	if reflect.DeepEqual(want, got) {
 		t.Fatalf("got %s, expected %s", got, want)
 	}
